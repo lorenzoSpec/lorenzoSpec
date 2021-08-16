825302daf04b08cbabce2b3721a5e1397df29314
lorenzoSpec <john.erwin.lorenzo.2002@gmail.com>
2021-08-16 17:51:00 +0800

    finished footer

--- js/gmail.js
+++
@@ -1,7 +0,0 @@
-const MYGMAIL =  document.getElementById('my-gmail');
-
-function copyToClipboard(text){
-  navigator.clipboard.writeText(text);
-}
-
-MYGMAIL.addEventListener('click', function(){copyToClipboard('john.erwin.lorenzo.2002@gmail.com')});
